34fe7d012dde4d88d1ce33e99ed2638a5f867bfa
Lucas Oliveira <lucasoft.dev@outlook.com>
2024-04-21 16:53:32 -0300

    feat: post, put and delete methods for coupon api

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -47,7 +48,7 @@ namespace Mango.Services.CouponAPI.Controllers
         {
             try
             {
-                var ret = db.Coupons.FirstOrDefault(u => u.CouponCode.ToLower() == code.ToLower());
+                var ret = db.Coupons.First(u => string.Equals(u.CouponCode, code));
                 _response.Result = mapper.Map<CouponDto>(ret);
             }
             catch (Exception e)
@@ -58,5 +59,60 @@ namespace Mango.Services.CouponAPI.Controllers
             return _response;
         }
 
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var coupon = mapper.Map<Coupon>(couponDto);
+                db.Coupons.Add(coupon);
+                db.SaveChanges();
+                _response.Result = mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.Message = e.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                var coupon = mapper.Map<Coupon>(couponDto);
+                db.Coupons.Update(coupon);
+                db.SaveChanges();
+                _response.Result = mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.Message = e.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                var coupon = db.Coupons.First(u => u.Id == id);
+                db.Coupons.Remove(coupon);
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.Message = e.Message;
+            }
+
+            return _response;
+        }
     }
 }
